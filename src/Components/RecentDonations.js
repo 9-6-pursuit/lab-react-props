cb2b33c325121850070549ff9c49b562d00155f9
tshering gurung <tshering@tsherings-air.myfiosgateway.com>
2023-04-20 11:39:39 -0400

    lab react props

--- src/Components/RecentDonations.js
+++ src/Components/RecentDonations.js
@@ -1,3 +1,17 @@
-export default function RecentDonations() {
-  return null;
+export default function RecentDonations({donation}) {
+  //  const individual = donation.forEach(el => {
+  //   return el
+  //  });
+  return (
+    <section>
+      <h2>Recent Donations</h2>
+      <ul>
+        <li><span>{donation[0].name} donated ${donation[0].amount}</span>{donation[0].caption}</li>
+        <li><span>{donation[1].name} donated ${donation[1].amount}</span>{donation[1].caption}</li>
+        <li><span>{donation[2].name} donated ${donation[2].amount}</span>{donation[2].caption}</li>
+        <li><span>{donation[3].name} donated ${donation[3].amount}</span>{donation[3].caption}</li>
+       
+      </ul>
+    </section>
+  );
 }
