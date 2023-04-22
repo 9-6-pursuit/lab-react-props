80cdc8f335a8a7f12853bba2c70ef8002a2241f9
Adrian Burke <114108495+AdrianBurke1@users.noreply.github.com>
2023-04-22 16:01:17 -0400

    lab-react-props-9.6

--- src/Components/Progress.js
+++ src/Components/Progress.js
@@ -1,3 +1,18 @@
-export default function Progress() {
-  return null;
+export default function Progress(props) {
+  const donations = props.donations;
+  let totalDonated = 0;
+  donations.forEach(donation => {
+    totalDonated += donation.amount;
+  });
+
+  return (
+    <>
+      <section className="progress">
+        <h2>
+          Raised <span className="secondary">${totalDonated}</span> of 
+          <span className="secondary"> ${props.targetAmount}</span>
+        </h2>
+      </section>
+    </>
+  );
 }
--- src/Components/RecentDonations.js
+++ src/Components/RecentDonations.js
@@ -1,3 +1,21 @@
-export default function RecentDonations() {
-  return null;
+export default function RecentDonations(props) {
+  const donations = props.donations;
+
+  return (
+    <>
+      <section>
+        <h2>Recent Donations</h2>
+        <ul>
+          {
+            donations.map(person => (
+                <li>
+                  <span>{person.name} donated ${person.amount}</span>
+                  {person.caption}
+                </li>
+            ))
+          }
+        </ul>
+      </section>
+    </>
+  );
 }
