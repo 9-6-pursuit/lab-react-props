aa1a0955a9e37d486bb6be1d6045da53f8125613
Starr <Starr@M-C02F21YMMD6R.nordstrom.net>
2023-04-21 23:09:32 -0400

    Props

--- src/Components/Progress.js
+++ src/Components/Progress.js
@@ -1,3 +1,13 @@
-export default function Progress() {
-  return null;
+export default function Progress({donations, target}) {
+  let total = 0
+  donations.forEach(donation => {
+    total += donation.amount
+  })
+  return (
+    <section className="progress">
+      <h2>
+        Raised <span className="secondary">${total}</span> of <span className="secondary">${target}</span>
+      </h2>
+    </section>
+  )
 }
--- src/Components/RecentDonations.js
+++ src/Components/RecentDonations.js
@@ -1,3 +1,11 @@
-export default function RecentDonations() {
-  return null;
+export default function RecentDonations({donations}) {
+  return (
+    <section>
+      <h2>Recent Donations</h2>
+      <ul>
+        {donations.map(recentDonor => <li><span>{recentDonor.name} donated ${recentDonor.amount}</span>{recentDonor.caption}</li>)}
+    </ul>
+  </section>
+  )
 }
+// Use the .map const donations = donations.map(amount => {name} donated {amount})
