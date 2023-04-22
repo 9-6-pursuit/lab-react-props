0d6bf24131ad951bef7891528e9ca4c8bfa47609
Joanavel Pascual <joanavelpascual@pursuit.org>
2023-04-22 14:51:38 -0400

    lab completed

--- src/Components/Progress.js
+++ src/Components/Progress.js
@@ -1,3 +1,10 @@
-export default function Progress() {
-  return null;
+export default function Progress({ donations }) {
+  const totalDonated = donations.reduce((acc, donation) => acc + donation.amount, 0);
+  return (
+    <section className="progress">
+      <h2>
+        Raised <span className="secondary">${totalDonated}</span> of <span className="secondary">$1000</span>
+      </h2>
+    </section>
+  );
 }
