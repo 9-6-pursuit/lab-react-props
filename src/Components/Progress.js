7c3e1c0b41698e884ca6aa97329e2870b4e48aee
Sabri Mohiuddin <you@example.com>
2023-04-22 14:11:28 -0400

    sabri-React Props Lab

--- src/Components/Progress.js
+++ src/Components/Progress.js
@@ -1,3 +1,23 @@
-export default function Progress() {
-  return null;
-}
+// export default function Progress() {
+//   return null;
+// }
+
+
+import React from 'react';
+
+const Progress = (props) => {
+  const raisedAmount = props.donations.reduce((total, donation) => total + donation.amount, 0);
+
+  return (
+    <section className="progress">
+      <h2>
+        Raised <span className="secondary">${raisedAmount}</span> of <span className="secondary">${props.targetAmount}</span>
+      </h2>
+    </section>
+  );
+};
+
+export default Progress;
+
+
+
