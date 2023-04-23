d566f46351628c9dde22be445b83c67bedf38b74
RK-404 <rizwan_k@RIZWANs-MacBook-Air.local>
2023-04-23 00:51:05 -0400

    Solved Lab

--- src/Components/Progress.js
+++ src/Components/Progress.js
@@ -1,3 +1,16 @@
-export default function Progress() {
-  return null;
+export default function Progress({donations, targetAmount}) {
+  const donationMade = donations => {
+    let total = 0;
+    donations.forEach(donation => total += donation.amount);
+    return total;
+  }
+
+  return (
+    <section className="progress">
+      <h2>
+        Raised <span className="secondary">${donationMade(donations)}</span> of
+        <span className="secondary"> ${targetAmount}</span>
+      </h2>
+    </section>
+  );
 }
--- src/Components/RecentDonations.js
+++ src/Components/RecentDonations.js
@@ -1,3 +1,10 @@
-export default function RecentDonations() {
-  return null;
+export default function RecentDonations({donations}) {
+  return (
+    <section>
+      <h2>Recent Donations</h2>
+      <ul>
+        {donations.map( ({name, caption, amount}) => <li><span>{name} donated ${amount}</span>{caption}</li>)}
+      </ul>
+    </section>
+  );
 }
