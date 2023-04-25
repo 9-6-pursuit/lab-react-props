c04d600da8bdf8ee939e9da8cbcb7782478e0d31
LELIAH <LELIAH791@GMAIL.COM>
2023-04-25 11:18:02 -0400

    props-propped

--- src/Components/Progress.js
+++ src/Components/Progress.js
@@ -1,3 +1,16 @@
-export default function Progress() {
-  return null;
+export default function Progress({donations, targetAmount}) {
+
+  let total = 0;
+  donations.map((donation) => {
+    return total = total + donation.amount;
+  })
+
+  return (
+    <section className="progress">
+  <h2>
+    Raised <span className="secondary">${total}</span> of 
+     <span className="secondary"> ${targetAmount}</span>
+  </h2>
+</section>
+  );
 }
