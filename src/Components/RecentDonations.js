ffbc2feef5dc447fee099ff5d831bcbe62590a1a
antunishdPursuit <dennysantunish@pursuit.org>
2023-04-27 00:27:35 -0400

    Completed

--- src/Components/RecentDonations.js
+++ src/Components/RecentDonations.js
@@ -1,3 +1,12 @@
-export default function RecentDonations() {
-  return null;
+export default function RecentDonations({dono}) {
+  return (
+    <section>
+      <h2>Recent Donations</h2>
+      <ul>
+        {dono.map((donation, index) => (
+          <li key={index}><span key={index}>{donation.name} donated ${donation.amount}</span>{donation.caption}.</li>
+        ) )}
+      </ul>
+    </section>
+  );
 }
