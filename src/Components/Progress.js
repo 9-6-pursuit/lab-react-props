776097f6d2f5671cb3aeee3e8918fc28742653a3
Frantz-Sebastien Mathias <frantz-sebastienmathias@pursuit.org>
2023-05-08 18:32:35 -0400

    I now have an idea on how props works

--- src/Components/Progress.js
+++ src/Components/Progress.js
@@ -1,3 +1,11 @@
-export default function Progress() {
-  return null;
+export default function Progress({donations}) {
+  const soFar = donations.reduce((addinUp, donation) => {
+    return addinUp + donation.amount
+  },0)
+  return <section className="progress">
+  <h2>
+    Raised <span className="secondary">${soFar}</span> of
+    <span className="secondary">$1000</span>
+  </h2>
+</section>;
 }
--- src/Components/RecentDonations.js
+++ src/Components/RecentDonations.js
@@ -1,3 +1,13 @@
-export default function RecentDonations() {
-  return null;
+export default function RecentDonations({donations}) {
+  return (
+    <section>
+      <h2>Recent Donations</h2>
+      <ul>
+    {donations.map((donation)=>{
+     return <li><span>{donation.name} donated ${donation.amount}</span>{donation.caption}</li>})}
+     </ul>
+    
+    </section>
+  )
 }
+
