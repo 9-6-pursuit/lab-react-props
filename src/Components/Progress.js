887e731faef0a1ff3fe71a00119a609b2205ea01
“irwinj7” <Irwinjorge@pursuit.com>
2023-06-05 23:40:23 -0400

    finsihed 100%

--- src/Components/Progress.js
+++ src/Components/Progress.js
@@ -1,3 +1,15 @@
-export default function Progress() {
-  return null;
+export default function Progress({donations, targetAmount}) {
+    let total = 0
+    for (let donation of donations){
+        total = (donation.amount + total)
+    }
+    return (
+        <section className="progress">
+            <h2>
+            Raised <span className="secondary">${total}</span> of<span className="secondary"> ${targetAmount}</span>
+            </h2>
+        </section>
+    )
 }
+
+// ${donations.map((donation)=>{ return (total = (donation.amount + total))})}
--- src/Components/RecentDonations.js
+++ src/Components/RecentDonations.js
@@ -1,3 +1,18 @@
-export default function RecentDonations() {
-  return null;
+export default function RecentDonations({donations}) {
+  console.log(donations)
+  return (
+    <section>
+      <h3>Recent Donations</h3>
+      <ul>
+        {donations.map((donation) => {
+          
+          return (
+            <li><span>{donation.name} donated ${donation.amount}</span>{donation.caption}</li>
+          )
+        })}
+        {/* <li><span>Jo donated $25</span>You really need this. Really.</li>
+        <li><span>Rami donated $10</span>Here, take a break from work!</li> */}
+      </ul>
+    </section>
+  );
 }
